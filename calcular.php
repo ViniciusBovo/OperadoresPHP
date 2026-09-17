a8230a5b6381a6a00f622f4ecf3337be7e410e04
Vinicius Bovo <viniciusliberattibovo@gmail.com>
2026-09-17 12:43:55 -0300

    Terceiro Commit

--- calcular.php
+++ calcular.php
@@ -6,11 +6,16 @@ $n2 = (float) $_POST["n2"];
 $adicao = $n1 + $n2;
 $subtracao = $n1 - $n2;
 $multiplicacao = $n1 * $n2;
-$divisao = $n1/$n2;
-$modulo = $n1 % $n2;
+
+$divisaovalida = $n2 !=0;
+if($divisaovalida==true)
+{
+    $divisao = $n1/$n2;
+    $modulo = $n1 % $n2;
+}
+
 $potencia = $n1 ** $n2;
 $concatenacao = $n1.$n2;
-
 
 ?>
 
@@ -19,7 +24,7 @@ $concatenacao = $n1.$n2;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Operações Aritméticas - AI Interface</title>
+    <title>Operações Aritméticas</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -37,7 +42,19 @@ $concatenacao = $n1.$n2;
             <?php echo "$n1 X $n2 = $multiplicacao"; ?>
         </h3>
         <h3>
-            <?php echo "$n1 ÷ $n2 = ".number_format($divisao,2,','); ?>
+            <?php 
+            if($divisaovalida==true)
+            {
+                echo "$n1 ÷ $n2 = ".number_format($divisao,2,',');
+                echo "<br>";
+                echo "$n1 módulo $n2 = ".number_format($divisao,2,',');
+            } 
+            else
+            {
+                echo "<h1>Não Há divisão por zero!</h1>";
+            } 
+
+        ?>        
         </h3>
         <h3>
             <?php echo "$n1 <sup> $n2 </sup> = ".number_format($potencia,2,','); ?>

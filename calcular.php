d7fdacdf365e320becb6bd49d46c38c733046db5
Vinicius Bovo <viniciusliberattibovo@gmail.com>
2026-09-17 12:06:27 -0300

    Segundo Commit

--- calcular.php
+++ calcular.php
@@ -42,6 +42,7 @@ $concatenacao = $n1.$n2;
         <h3>
             <?php echo "$n1 <sup> $n2 </sup> = ".number_format($potencia,2,','); ?>
         </h3>
+        <a href="index.php">Voltar</a>
     </div>
 </body>
 </html>
